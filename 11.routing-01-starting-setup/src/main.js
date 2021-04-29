5f64556f0b2d4b1b899619a681077a60e34b6d7f
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-04-29 19:09:23 -0300

    .

---
+++ 11.routing-01-starting-setup/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList';
+import TeamMembers from './components/teams/TeamMembers';
+import UsersList from './components/users/UsersList';
+
+const router = createRouter({
+  history: createWebHistory(),
+  linkActiveClass: 'active',
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/teams/:teamId', component: TeamMembers },
+    { path: '/users', component: UsersList }
+  ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');

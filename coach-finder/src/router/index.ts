6479d73a17e965cd56251262031cc93c9597dc71
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-05-12 15:35:54 -0300

    register routes

--- coach-finder/src/router/index.ts
+++ coach-finder/src/router/index.ts
@@ -1,24 +1,49 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    redirect: '/coaches',
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/coaches',
+    component: null,
   },
+  {
+    path: '/coaches/:id',
+    component: null,
+    children: [
+      {
+        path: 'contact',
+        component: null,
+      },
+    ],
+  },
+  {
+    path: '/register',
+    component: null,
+  },
+  {
+    path: '/requests',
+    component: null,
+  },
+  {
+    path: '/:notFound(.*)',
+    component: null,
+  },
+
+  // {
+  // path: '/about',
+  // name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 

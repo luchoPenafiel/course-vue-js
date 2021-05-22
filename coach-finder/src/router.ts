4233ae19b5d5e9c6be99feb7f5ee66cc6b485d40
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2021-05-22 20:47:33 -0300

    coach finder: add navigation guards

--- coach-finder/src/router.ts
+++ coach-finder/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 import CoachesList from '@/views/CoachesList.vue';
+import store from './store';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -25,14 +26,23 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/register',
     component: () => import('@/views/CoachRegistration.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/requests',
     component: () => import('@/views/Requests.vue'),
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/login',
     component: () => import('@/views/UserAuth.vue'),
+    meta: {
+      requiresUnauth: true,
+    },
   },
   {
     path: '/:notFound(.*)',
@@ -43,6 +53,16 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.getters.isAuth) {
+    next('/login');
+  } else if (to.meta.requiresUnauth && store.getters.isAuth) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
